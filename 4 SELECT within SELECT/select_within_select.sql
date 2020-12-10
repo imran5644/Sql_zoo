4bc8115f21e277a331fa534bc029d3f640443e4e
imran5644 <imi56444@gmail.com>
2020-12-10 21:30:17 +0530

    updated select within select

--- 4 SELECT within SELECT/select_within_select.sql
+++ 4 SELECT within SELECT/select_within_select.sql
@@ -21,5 +21,31 @@ AND population < (SELECT population FROM world WHERE name = 'Poland')
 
 
 SELECT name, CONCAT(ROUND(population/(SELECT population FROM world 
-    WHERE name = 'Germany')*100), '%') FROM world WHERE continent = 'Europe';
+    WHERE name = 'Germany')*100), '%') FROM world WHERE continent = 'Europe'
+
+
+SELECT name FROM world
+ WHERE gdp > ALL(SELECT gdp FROM world
+                          WHERE gdp>0 and continent = 'Europe')
+
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+
+
+SELECT continent, name from world x
+    WHERE name <= All(select name from world y where x.continent = y.continent) 
+
+
+SELECT name, continent, population FROM world 
+    WHERE continent IN (SELECT continent FROM world  x WHERE 25000000 >= (SELECT MAX(population) FROM world y 
+        WHERE x.continent = y.continent))
+
+
+SELECT name, continent from world x 
+    WHERE population > All(select 3*population from world y 
+        WHERE x.continent = y.continent and x.name <> y.name)
 
